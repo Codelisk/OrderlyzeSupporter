8631245cea7ffa5ff62fdccc834e2f3cc297bb1c
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-01-11 15:44:49 +0100

    Update again

--- src/Shared/Backend.Api/Constants.cs
+++ src/Shared/Backend.Api/Constants.cs
@@ -8,6 +8,6 @@ namespace Backend.Api
 {
     public static class Constants
     {
-        public static string RestUrl = "https://41da-178-189-140-192.ngrok-free.app/";
+        public static string RestUrl = "https://729a-178-189-140-192.ngrok-free.app/";
     }
 }
